6bd4ed74c575398e045798c81f0b7e3ed7989f09
pieroq09 <pieroq329@gmail.com>
2026-04-23 13:28:45 -0500

    Moviendo archivos a la subcarpeta mi_app correctamente

---
+++ mi_app/mi_app/lib/producto.dart
@@ -0,0 +1,13 @@
+class Producto {
+  final String nombre;
+  final String descripcion;
+  final double precio;
+  final String imagen;
+
+  Producto({
+    required this.nombre,
+    required this.descripcion,
+    required this.precio,
+    required this.imagen,
+  });
+}
